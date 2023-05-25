2d62dc03f527a8b43d2ae3babb6c7aae24efccf2
Aimergen <adkuhidan@gmail.com>
2023-05-25 18:43:11 +0800

    facebook chat yanzlav

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -4,12 +4,14 @@ import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { Searchcontext } from "@/components/context/Searchcontext";
 import { ToastContainer } from "react-toastify";
+import FacebookCustomerChat from "@/components/FacebookCustomerChat";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CurrentUserProvider>
       <Searchcontext>
         <Component {...pageProps} />
+        <FacebookCustomerChat />
         <ToastContainer />
       </Searchcontext>
     </CurrentUserProvider>
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -78,7 +78,6 @@ export default function Home({ data }: { data: IProduct[] }) {
         <Reason />
         {/* </div>
       <Footer /> */}
-        {/* <FacebookCustomerChat /> */}
       </Layout>
     </>
   );
